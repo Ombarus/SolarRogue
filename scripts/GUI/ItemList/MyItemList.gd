f9e9524bb478f3a6cc2b7d39721d9e377fe766a9
ombarus <ombarus.dev@gmail.com>
2019-04-24 11:12:45 +0900

    renamed temp_2 scene to scanner_fx. Started working on porting all my lists to my generic custom list. First, selectable lists

--- scripts/GUI/ItemList/MyItemList.gd
+++ scripts/GUI/ItemList/MyItemList.gd
@@ -3,14 +3,16 @@ extends Control
 # A scene that will be duplicated to add rows to the list
 export(PackedScene) var Row
 export(String) var DragDropID = ""
+export(ButtonGroup) var SelectGroup = null
 
 # takes an array of dictionnary information for row initialization
 # returns an array of dictionary with information about row content
 var Content setget set_content, get_content
 
+
 signal OnDragDropCompleted(origin_data, destination_data)
 
-var _debug = false
+var _debug = true
 onready var _rows_node = get_node("Rows")
 
 func Clear():
@@ -25,6 +27,7 @@ func set_content(val):
 		var n = Row.instance()
 		_rows_node.add_child(n)
 		row["origin"] = self
+		row["group"] = SelectGroup
 		row["index"] = count
 		row["dragdrop_id"] = DragDropID
 		count += 1
@@ -39,6 +42,8 @@ func get_content():
 func _ready():
 	if _debug == true:
 		set_content([{"text":"hello"}, {"text":"world"}, {"text":"bleh"}])
+
+########### DRAG & DROP ###############
 
 func bubble_drop(orig_data, dst_data):
 	emit_signal("OnDragDropCompleted", orig_data, dst_data)
