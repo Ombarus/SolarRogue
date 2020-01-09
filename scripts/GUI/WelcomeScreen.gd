87996c84002f2979491965eb466eac31894e2f1b
ombarus <ombarus.dev@gmail.com>
2020-01-09 11:32:26 +0900

    Translated WelcomeScreen like I did the Tutorial

--- scripts/GUI/WelcomeScreen.gd
+++ scripts/GUI/WelcomeScreen.gd
@@ -1,5 +1,7 @@
 extends "res://scripts/GUI/GUILayoutBase.gd"
 
+export(NodePath) var DefaultLang = "base/en"
+
 func _ready():
 	get_node("base").connect("OnOkPressed", self, "Ok_Callback")
 	
@@ -14,4 +16,17 @@ func Init(init_param):
 	get_node("base").disabled = false
 	var player_name = init_param["player_name"]
 	get_node("base").title = Globals.mytr("Welcome %s...", [player_name])
+	
+	var locale := TranslationServer.get_locale()
+	var contents : Array = get_tree().get_nodes_in_group("welcome_lang")
+	var found := false
+	for content in contents:
+		if locale == content.name:
+			found = true
+			content.visible = true
+		else:
+			content.visible = false
+			
+	if found == false:
+		get_node(DefaultLang).visible = true
 	
