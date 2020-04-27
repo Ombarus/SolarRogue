ed5da09f3eb2c10540465ea7c3dc3ed611129cb0
ombarus <ombarus.dev@gmail.com>
2020-04-27 17:06:48 +0900

    Moved art assets to a private repo. My version of drm :)

--- scripts/GUI/Audio/ToggleBtnAudio.gd
+++ scripts/GUI/Audio/ToggleBtnAudio.gd
@@ -6,12 +6,12 @@ var _click := AudioStreamPlayer.new()
 func _ready():
 	self.connect("mouse_entered", self, "mouse_entered_Callback")
 	self.connect("pressed", self, "pressed_Callback")
-	_hover.stream = preload("res://data/sounds/sfx/btn/hover2.wav")
+	_hover.stream = preload("res://data/private/sounds/sfx/btn/hover2.wav")
 	_hover.volume_db = -20.0
 	_hover.bus = "Sfx"
 	self.call_deferred("add_child", _hover)
 	
-	_click.stream = preload("res://data/sounds/sfx/btn/click.wav")
+	_click.stream = preload("res://data/private/sounds/sfx/btn/click.wav")
 	_click.bus = "Sfx"
 	self.call_deferred("add_child", _click)
 
