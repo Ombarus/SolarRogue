7346709f9dfa57f7ded8f6ccb202bcebfb9170eb
ombarus <ombarus.dev@gmail.com>
2021-10-11 14:44:13 +0900

    Updated export scripts

--- sign.bat
+++ sign.bat
@@ -1,9 +1,9 @@
 pushd %~dp0
 
-copy SolarRogue%1.apk F:\Dev\Projects\GooglePlay
-cd F:\Dev\Projects\GooglePlay
-"C:\Program Files\Java\jdk1.8.0_181\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore SolarRogue%1.apk android-app-key
-"C:\Program Files (x86)\Android\android-sdk\build-tools\23.0.1\zipalign.exe" -v 4 SolarRogue%1.apk SolarRogue%1-signed.apk
+copy SolarRogue%1.apk D:\Dev\Projects\GooglePlay
+cd D:\Dev\Projects\GooglePlay
+"C:\Program Files\Java\jdk-17\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore SolarRogue%1.apk android-app-key
+"C:\Users\match\AppData\Local\Android\Sdk\build-tools\31.0.0\zipalign.exe" -v 4 SolarRogue%1.apk SolarRogue%1-signed.apk
 move SolarRogue%1-signed.apk "%~dp0\SolarRogue%1-signed.apk"
 
 del SolarRogue%1.apk
